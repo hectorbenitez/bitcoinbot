2d1ac101432093d80b8d2a41e82c89b6ec87f8fa
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-01-23 17:19:34 -0600

    fb webhook

--- app.js
+++ app.js
@@ -1,6 +1,62 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+var bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser().json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "SECRET"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
